8f3bbe6794974a482c265e920604df4e97ed0829
sarahjpell <32115290+sarahjpell@users.noreply.github.com>
2019-12-01 16:22:14 -0500

    community thing 8-)

--- workingWithBlocks.py
+++ workingWithBlocks.py
@@ -1,19 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 29 16:03:12 2019
-
-@author: samic
-"""
-
+# # -*- coding: utf-8 -*-
+# """
+# Created on Fri Nov 29 16:03:12 2019
+#
+# @author: samic
+# """
+#
 import networkx as nx
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+# from networkx.algorithms import community
+import community
+# import networkx as nx
+# from networkx.algorithms import community
 
-
-#block = pd.read_csv('block1.csv')
-#block = pd.read_csv('block2.csv')
-#block = pd.read_csv('block3.csv')
+# block = pd.read_csv('block1.csv')
+# block = pd.read_csv('block2.csv')
+# block = pd.read_csv('block3.csv')
 block = pd.read_csv('block4.csv')
 
 block.drop(block.columns[[1]], axis=1, inplace=True)
@@ -27,7 +30,13 @@ toGraph.columns = ['var1', 'var2','value']
 toGraph_filtered = toGraph.loc[ (toGraph['value'] > .8) & (toGraph['var1'] != toGraph['var2']) ]
 G = nx.from_pandas_edgelist(toGraph_filtered, 'var1', 'var2')
 
-#g = nx.draw(G, node_size=10)
+# g = nx.draw(G, node_size=10)
+# parts = community.best_partition(G)
+# values = [parts.get(node) for node in G.nodes()]
 
-# centrality
-nx.eigenvector_centrality(G)
+#this works???
+communities_generator = community.girvan_newman(G)
+top_level_communities = next(communities_generator)
+next_level_communities = next(communities_generator)
+# print(sorted(map(sorted, next_level_communities)))
+print(next_level_communities)
